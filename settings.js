847e015aef174d75d9183f079cbabaf9a4d1090c
sithummm <sithummihiranga228@gmail.com>
2025-04-17 17:53:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {
-
+module.exports = { 
+    
 SESSION_ID: process.env.SESSION_ID || "LAKA-MD=N4I3kJ6Q#t9TXErykbHjzNUz1VxlTWJ4P76nteL5zX3MBBKkQQbw
     ",
 PREFIX: process.env.PREFIX || '.' ,
